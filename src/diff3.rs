8da16551ea1644a960746d77644d36703bf2ea01
Aaron Bentley <aaron@aaronbentley.com>
2025-07-28 21:48:15 -0400

    Handle right-hand matches a bit.

--- src/diff3.rs
+++ src/diff3.rs
@@ -1,11 +1,11 @@
 use diff::Result as DiffResult;
 
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum MatchingVersions {
-    All,
-    MyOld,
+    MyOldYour,
     OldYour,
     MyYour,
+    MyOld,
     My,
     Old,
     Your,
@@ -17,16 +17,41 @@ struct Line<T: PartialEq>  {
     versions: MatchingVersions
 }
 
-fn match_sequence<'a, T: PartialEq>(myfile: &'a [T], oldfile:  &'a [T], _yourfile:  &'a [T]) -> Vec<Line<&'a T>> {
+fn match_sequence<'a, T: PartialEq + std::fmt::Debug>(myfile: &'a [T], oldfile:  &'a [T], yourfile:  &'a [T]) -> Vec<Line<&'a T>> {
     use diff::Result::*;
     use MatchingVersions::*;
     let mut output = vec![];
+    let mut old_your = diff::slice(oldfile, yourfile).into_iter();
     for result in diff::slice(myfile, oldfile) {
-        output.push(match result {
-            Left(line) => Line {line, versions: My},
-            Both(line, _) => Line {line, versions: MyOld},
-            Right(line) => Line {line, versions: Old},
-        })
+        let (maybe_combiner, line) = match result {
+            Left(line) => (None, line),
+            Right(line) => (Some(false), line),
+            Both(line, _) => (Some(true), line),
+        };
+        if let Some(combiner) = maybe_combiner {
+            for r_line in &mut old_your {
+                let versions = match r_line {
+                    Right(x) => {
+                        output.push(Line{line: x, versions: Your});
+                        continue
+                    },
+                    Left(_) if combiner => MyOld,
+                    Left(_) => Old,
+                    Both(_,_) if combiner => MyOldYour,
+                    Both(_,_) => OldYour,
+                };
+                output.push(Line{line, versions});
+                break
+            }
+        } else {
+            output.push(Line{line, versions: My});
+        }
+    }
+    for result in old_your {
+        let Right(line) = result else {
+            panic!("Should not have anything other than right lines or we missed a match.  Got{:?}", result);
+        };
+        output.push(Line{line: line, versions: Your});
     }
     output
 }
@@ -37,12 +62,33 @@ mod tests {
     use super::*;
     use diff::Result as DiffResult;
     use MatchingVersions::*;
+
+    fn input(ink: &str) -> Vec<char>{
+        ink.chars().collect()
+    }
+
+    #[test]
+    fn test_match_sequence_no_yours() {
+        assert_eq!(vec![
+            Line {line: &'a', versions: Old},
+            Line {line: &'b', versions: MyOld},
+            Line {line: &'c', versions: My},
+        ], match_sequence(&input("bc"), &input("ab"), &[]))
+    }
     #[test]
     fn test_match_sequence() {
         assert_eq!(vec![
-            Line {line: &"a", versions: Old},
-            Line {line: &"b", versions: MyOld},
-            Line {line: &"c", versions: My},
-        ], match_sequence(&["b", "c"], &["a", "b"], &[]))
+            Line {line: &'a', versions: OldYour},
+            Line {line: &'b', versions: MyOldYour},
+            Line {line: &'c', versions: My},
+        ], match_sequence(&input("bc"), &input("ab"), &input("ab")))
+    }
+    #[test]
+    fn test_match_ends_in_your() {
+        assert_eq!(vec![
+            Line {line: &'a', versions: MyOldYour},
+            Line {line: &'b', versions: Your},
+            Line {line: &'c', versions: Your},
+        ], match_sequence(&input("a"), &input("a"), &input("abc")))
     }
 }
