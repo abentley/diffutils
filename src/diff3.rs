d0744aca1bff446329ee8eb2224048f2fc429872
Aaron Bentley <aaron@aaronbentley.com>
2025-08-07 19:51:26 -0400

    Implement filename errors

--- src/diff3.rs
+++ src/diff3.rs
@@ -1,6 +1,8 @@
 use std::env::ArgsOs;
-use std::process::{ExitCode};
+use std::ffi::OsString;
+use std::fmt::Display;
 use std::iter::Peekable;
+use std::process::ExitCode;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum MatchingVersions {
@@ -14,13 +16,16 @@ enum MatchingVersions {
 }
 
 #[derive(Debug, PartialEq)]
-struct Line<T: PartialEq>  {
+struct Line<T: PartialEq> {
     line: T,
-    versions: MatchingVersions
+    versions: MatchingVersions,
 }
 
 fn match_sequence<'a, T: PartialEq + std::fmt::Debug>(
-    myfile: &'a [T], oldfile:  &'a [T], yourfile:  &'a [T]) -> Vec<Line<&'a T>> {
+    myfile: &'a [T],
+    oldfile: &'a [T],
+    yourfile: &'a [T],
+) -> Vec<Line<&'a T>> {
     use diff::Result::*;
     use MatchingVersions::*;
     let mut output = vec![];
@@ -38,27 +43,36 @@ fn match_sequence<'a, T: PartialEq + std::fmt::Debug>(
                 let versions = match r_line {
                     Right(x) => {
                         right_lines.push(x);
-                        continue
-                    },
+                        continue;
+                    }
                     Left(_) if has_my => MyOld,
                     Left(_) => Old,
-                    Both(_,_) if has_my => MyOldYour,
-                    Both(_,_) => OldYour,
+                    Both(_, _) if has_my => MyOldYour,
+                    Both(_, _) => OldYour,
                 };
                 eprintln!("ll {:?}", left_lines);
                 eprintln!("rl {:?}", right_lines);
                 eprintln!("ver {:?}", versions);
                 for sides_result in diff::slice(&left_lines, &right_lines) {
                     output.push(match sides_result {
-                        Left(line) => Line{line: *line, versions: My},
-                        Right(line) => Line{line: *line, versions: Your},
-                        Both(line, _) => Line{line: *line, versions: MyYour},
+                        Left(line) => Line {
+                            line: *line,
+                            versions: My,
+                        },
+                        Right(line) => Line {
+                            line: *line,
+                            versions: Your,
+                        },
+                        Both(line, _) => Line {
+                            line: *line,
+                            versions: MyYour,
+                        },
                     });
                 }
                 left_lines.clear();
                 right_lines.clear();
-                output.push(Line{line, versions});
-                break
+                output.push(Line { line, versions });
+                break;
             }
         } else {
             left_lines.push(line)
@@ -66,7 +80,10 @@ fn match_sequence<'a, T: PartialEq + std::fmt::Debug>(
     }
     for result in &mut old_your {
         let Right(x) = result else {
-            panic!("Should not have anything other than right lines or we missed a match.  Got{:?}", result);
+            panic!(
+                "Should not have anything other than right lines or we missed a match.  Got{:?}",
+                result
+            );
         };
         right_lines.push(x);
     }
@@ -74,23 +91,63 @@ fn match_sequence<'a, T: PartialEq + std::fmt::Debug>(
     eprintln!("rl {:?}", right_lines);
     for sides_result in diff::slice(&left_lines, &right_lines) {
         output.push(match sides_result {
-            Left(line) => Line{line: *line, versions: My},
-            Right(line) => Line{line: *line, versions: Your},
-            Both(line, _) => Line{line: *line, versions: MyYour},
+            Left(line) => Line {
+                line: *line,
+                versions: My,
+            },
+            Right(line) => Line {
+                line: *line,
+                versions: Your,
+            },
+            Both(line, _) => Line {
+                line: *line,
+                versions: MyYour,
+            },
         });
     }
     output
 }
 
-pub fn main(opts: Peekable<ArgsOs>) -> ExitCode {
+enum Error {
+    MissingOperand,
+}
+
+impl Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match &self {
+            Error::MissingOperand => {
+                write!(fmt, "missing operand")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+fn next_file<T: Iterator<Item = OsString>>(opts_iter: &mut T) -> Result<OsString, Error> {
+    let Some(x) = opts_iter.next() else {
+        return Err(Error::MissingOperand);
+    };
+    Ok(x)
+}
+
+fn real_main(opts: Peekable<ArgsOs>) -> Result<(), Error> {
     let opts: Vec<_> = opts.collect();
     let mut opts_iter = opts.into_iter();
     opts_iter.next();
-    let mine = opts_iter.next().unwrap();
-    let old = opts_iter.next().unwrap();
-    let theirs = &opts_iter.next().unwrap();
+    let mine = next_file(&mut opts_iter)?;
+    let old = next_file(&mut opts_iter)?;
+    let theirs = next_file(&mut opts_iter)?;
     eprintln!("{:?} {:?} {:?}", mine, old, theirs);
-    ExitCode::from(0)
+    Ok(())
+}
+
+pub fn main(opts: Peekable<ArgsOs>) -> ExitCode {
+    if let Err(e) = real_main(opts) {
+        eprintln!("{e}");
+        ExitCode::from(1)
+    } else {
+        ExitCode::from(0)
+    }
 }
 
 #[cfg(test)]
@@ -98,39 +155,84 @@ mod tests {
     use super::*;
     use MatchingVersions::*;
 
-    fn input(ink: &str) -> Vec<char>{
+    fn input(ink: &str) -> Vec<char> {
         ink.chars().collect()
     }
 
     #[test]
     fn test_match_sequence_no_yours() {
-        assert_eq!(vec![
-            Line {line: &'a', versions: Old},
-            Line {line: &'b', versions: MyOld},
-            Line {line: &'c', versions: My},
-        ], match_sequence(&input("bc"), &input("ab"), &[]))
+        assert_eq!(
+            vec![
+                Line {
+                    line: &'a',
+                    versions: Old
+                },
+                Line {
+                    line: &'b',
+                    versions: MyOld
+                },
+                Line {
+                    line: &'c',
+                    versions: My
+                },
+            ],
+            match_sequence(&input("bc"), &input("ab"), &[])
+        )
     }
     #[test]
     fn test_match_sequence() {
-        assert_eq!(vec![
-            Line {line: &'a', versions: OldYour},
-            Line {line: &'b', versions: MyOldYour},
-            Line {line: &'c', versions: My},
-        ], match_sequence(&input("bc"), &input("ab"), &input("ab")))
+        assert_eq!(
+            vec![
+                Line {
+                    line: &'a',
+                    versions: OldYour
+                },
+                Line {
+                    line: &'b',
+                    versions: MyOldYour
+                },
+                Line {
+                    line: &'c',
+                    versions: My
+                },
+            ],
+            match_sequence(&input("bc"), &input("ab"), &input("ab"))
+        )
     }
     #[test]
     fn test_match_ends_in_your() {
-        assert_eq!(vec![
-            Line {line: &'a', versions: MyOldYour},
-            Line {line: &'b', versions: Your},
-            Line {line: &'c', versions: Your},
-        ], match_sequence(&input("a"), &input("a"), &input("abc")))
+        assert_eq!(
+            vec![
+                Line {
+                    line: &'a',
+                    versions: MyOldYour
+                },
+                Line {
+                    line: &'b',
+                    versions: Your
+                },
+                Line {
+                    line: &'c',
+                    versions: Your
+                },
+            ],
+            match_sequence(&input("a"), &input("a"), &input("abc"))
+        )
     }
     #[test]
     fn test_match_my_your() {
-        assert_eq!(vec![
-            Line {line: &'b', versions: MyYour},
-            Line {line: &'c', versions: MyOldYour},
-        ], match_sequence(&input("bc"), &input("c"), &input("bc")))
+        assert_eq!(
+            vec![
+                Line {
+                    line: &'b',
+                    versions: MyYour
+                },
+                Line {
+                    line: &'c',
+                    versions: MyOldYour
+                },
+            ],
+            match_sequence(&input("bc"), &input("c"), &input("bc"))
+        )
     }
 }
